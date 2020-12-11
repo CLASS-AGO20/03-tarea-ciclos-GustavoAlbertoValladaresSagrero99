fcf0e35256a1f35d1ef80e89004d2c249a94c837
Gustavo Alberto Valladares Sagrero <gvalladares@ucol.mx>
2020-12-10 23:50:12 -0600

    Updated already implemented obtenerImpares() function

--- src/main.js
+++ src/main.js
@@ -74,36 +74,18 @@ export default class App {
         {
           for(let i= numero1; i >= numero2; i--)
           {
-            if (i==numero2)
+            if (i%2 != 0)
             {
-              if (i%2 != 0)
-              {
-                impares+=i;
-              }
-            }else
-            {
-              if (i%2 != 0)
-              {
-                impares+=i+",";
-              }
+              impares+=i+",";
             }
           }
         }else if (numero2>numero1)
         {
           for(let i= numero2; i >= numero1; i--)
           {
-            if(i==numero1)
-            {
-              if (i%2 != 0)
-              {
-                impares+=i;
-              }              
-            }else
-            {
-              if (i%2 != 0)
+            if (i%2 != 0)
             {
               impares+=i+",";
-            }
             }
           }         
         }else{
